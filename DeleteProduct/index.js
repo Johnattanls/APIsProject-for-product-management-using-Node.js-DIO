f73b57507b2d6965c85c1a5315a73a2953ef34d7
johnattanls <johnattan.santana@gmail.com>
2021-02-15 18:51:58 -0300

    Projeto de API usando arquitetura serveless incluso, guiado por Igor Halfeld

---
+++ DeleteProduct/index.js
@@ -0,0 +1,16 @@
+const{ObjectId}=require('mongodb');
+const createMongoClient=require('../shared/mongoClient')
+
+module.exports = async function (context, req) {
+    const{id}=red.params;
+
+    const{client:MongoClient, closeConnectionFn} = await cr
+    const Products=MongoClient.collection('products');
+    const rest=await Products.findOneAndDelete({_id: ObjectId(id)});
+    
+    closeConnectionFn();
+    context.res={
+        status:200,
+        body:res,
+    }
+};
